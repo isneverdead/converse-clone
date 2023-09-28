21adecc89d39f2dc1995e35b528b04bcd4c1f665
isneverdead <richart135@gmail.com>
2023-09-29 02:43:55 +0700

    initial commit

---
+++ app/api/scrap/route.ts
@@ -0,0 +1,103 @@
+
+import { NextResponse } from 'next/server'
+import * as cheerio from 'cheerio';
+
+export async function GET(request: any) {
+
+
+
+
+    const response = await fetch(`https://www.converse.com/shop/womens-shoes?srule=Featured&start=35&sz=60`)
+    const html = await response.text()
+    const $ = cheerio.load(html);
+
+    const shoeTitle: any = [];
+    // iterating every card products
+    [...$('.plp-grid__item')].map((element: any) => {
+
+        // get into product container
+        // [...$(element).find(".product-tile")].map(async (item: any) => {
+        //     const title = $(item).find(".product-tile__url").text().replace(/\n/g, '')
+        //     const price = $(item).find(".product-price--sales").text().replace(/\n/g, '')
+        //     const type = $(item).find(".product-tile__secondary-badge").text().replace(/\n/g, '');
+        //     const productLink = $(item).find(".product-tile__img-url").attr("href");
+
+        //     let imageList: any = [];
+        //     [...$(item).find(".product-tile__img-container")].map((picture: any) => {
+        //         const image = $(picture).find('img').attr("data-src")
+
+
+        //         if (!image?.includes("medium_noimage")) {
+
+        //             imageList.push(image)
+        //         }
+
+        //     })
+        //     shoeTitle.push({ title, price, type, imageList, productLink })
+        //     // shoeTitle.push({ title })
+        // });
+        const productTile = $(element).find(".product-tile");
+        const title = $(productTile).find(".product-tile__url").text().replace(/\n/g, '')
+        const price = $(productTile).find(".product-price--sales").text().replace(/\n/g, '')
+        const type = $(productTile).find(".product-tile__secondary-badge").text().replace(/\n/g, '');
+        const productLink = $(productTile).find(".product-tile__img-url").attr("href");
+
+        let imageList: any = [];
+        [...$(productTile).find(".product-tile__img-container")].map((picture: any) => {
+            const image = $(picture).find('img').attr("data-src")
+
+
+            if (!image?.includes("medium_noimage") && !imageList.includes(image)) {
+
+                imageList.push(image)
+            }
+
+        })
+        shoeTitle.push({ title, price, type, imageList, productLink })
+        // [...$(element).find(".product-tile")].map(async (item: any) => {
+
+        //     // shoeTitle.push({ title })
+        // })
+        // return temp
+    })
+
+    // console.log($('.plp-grid__item').text());
+    function cleaning(array: any[], uniqueKeyProp: string) {
+        const allKeyProps = array.map((item: any) => {
+            return item[uniqueKeyProp]
+        })
+        // console.log(allKeyProps)
+        let temp: any = [];
+        allKeyProps.map((item: string, index: number) => {
+            if (allKeyProps.indexOf(item) == index) {
+                temp.push(array[index])
+            }
+        })
+        return temp
+    }
+
+    const cleanedShoeTitle = cleaning(shoeTitle, "title")
+    // console.log("shoe title length : " + shoeTitle.length)
+    // console.log("cleaned shoe title length : " + cleanedShoeTitle.length)
+
+    // const onlyTitle = cleanedShoeTitle.map((item: any) => item.title)
+    // return NextResponse.json({ onlyTitle, length: cleanedShoeTitle.length })
+    return NextResponse.json({ products: cleanedShoeTitle, length: cleanedShoeTitle.length })
+}
+
+export async function POST(request: any) {
+    const {
+        title, description, level, link
+    } = await request.json()
+
+    const newCourse: any = {
+        // id: uuidv4(),
+        title,
+        description,
+        level,
+        link
+    }
+    // courses.push(newCourse)
+    console.log({ title, description, level, link })
+    return NextResponse.json({})
+}
